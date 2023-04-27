0ee81a7ff8f3ad5dfb5776737bb37d891d5ee483
zidniryi123 <zidniryi123@gmail.com>
2023-04-27 22:38:36 +0700

    finish basic ui for app

--- App.tsx
+++ App.tsx
@@ -1,59 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
+  Text,
+  TouchableOpacity,
+  Image
 } from 'react-native';
+import { responsiveHeight, responsiveWidth } from "react-native-responsive-dimensions";
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const CARD_DATA = [
+  {
+    id: 1,
+    image: 'First Item',
+    count: 10
+  },
+  {
+    id: 2,
+    image: 'First Item',
+    count: 10
+  },
+  {
+    id: 3,
+    image: 'First Item',
+    count: 10
+  },
+];
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -63,56 +42,166 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.container}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <View style={styles.viewRow}>
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.button}>
+            <Text style={styles.textButton}>Like All</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.button}>
+            <Text style={styles.textButton}>Like All</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.button}>
+            <Text style={styles.textButton}>Like All</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      {/* Card */}
+      <View style={styles.containerList}>
+        <View style={styles.viewCard}>
+          <Image style={styles.imageCard} source={{ uri: "https://cdn.pixabay.com/photo/2023/04/23/19/29/daisies-7946594_1280.jpg" }} />
+          <View style={styles.viewRowCard}>
+            <View style={styles.viewLike}>
+              <TouchableOpacity activeOpacity={0.5}>
+                <View style={styles.button}>
+                  <Text style={styles.textButton}>Like All</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View style={styles.button}>
+                <Text style={styles.textButton}>Like All</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View style={styles.button}>
+                <Text style={styles.textButton}>Like All</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-      </ScrollView>
+
+        <View style={styles.viewCard}>
+          <Image style={styles.imageCard} source={{ uri: "https://cdn.pixabay.com/photo/2023/04/23/19/29/daisies-7946594_1280.jpg" }} />
+          <View style={styles.viewRowCard}>
+            <View style={styles.viewLike}>
+              <TouchableOpacity activeOpacity={0.5}>
+                <View style={styles.button}>
+                  <Text style={styles.textButton}>Like All</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View style={styles.button}>
+                <Text style={styles.textButton}>Like All</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View style={styles.button}>
+                <Text style={styles.textButton}>Like All</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.viewCard}>
+          <Image style={styles.imageCard} source={{ uri: "https://cdn.pixabay.com/photo/2023/04/23/19/29/daisies-7946594_1280.jpg" }} />
+          <View style={styles.viewRowCard}>
+            <View style={styles.viewLike}>
+              <TouchableOpacity activeOpacity={0.5}>
+                <View style={styles.button}>
+                  <Text style={styles.textButton}>Like All</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View style={styles.button}>
+                <Text style={styles.textButton}>Like All</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View style={styles.button}>
+                <Text style={styles.textButton}>Like All</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F4F4',
+    marginTop: responsiveHeight(10)
   },
-  sectionTitle: {
-    fontSize: 24,
+  viewRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: responsiveWidth(7),
+    marginRight: responsiveWidth(7)
+  },
+  button: {
+    width: responsiveWidth(25),
+    height: responsiveHeight(4),
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textButton: {
     fontWeight: '600',
+    color: 'white'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  containerList: {
+    marginTop: responsiveHeight(2),
   },
-  highlight: {
-    fontWeight: '700',
+  viewCard: {
+    width: responsiveWidth(88),
+    height: responsiveHeight(33),
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    marginBottom: responsiveHeight(2)
   },
+  imageCard: {
+    width: responsiveWidth(88),
+    height: responsiveHeight(26),
+    borderTopEndRadius: 10,
+    borderTopLeftRadius: 10
+  },
+  viewRowCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: responsiveWidth(2),
+    marginRight: responsiveWidth(2),
+    marginTop: responsiveHeight(1.5)
+  },
+  viewLike: {
+    marginRight: responsiveWidth(7)
+  }
 });
 
 export default App;
