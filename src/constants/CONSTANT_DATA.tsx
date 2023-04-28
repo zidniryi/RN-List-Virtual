36441bde75c0b03bb6a5dc51ea349f9161acb2c3
zidniryi123 <zidniryi123@gmail.com>
2023-04-28 11:09:55 +0700

    update all initial like value is 0

--- src/constants/CONSTANT_DATA.tsx
+++ src/constants/CONSTANT_DATA.tsx
@@ -2,7 +2,7 @@ const CONSTANT_DATA = [
     {
         id: 1,
         image: 'https://cdn.pixabay.com/photo/2023/04/23/19/29/daisies-7946594_1280.jpg',
-        count: 100
+        count: 0
     },
     {
         id: 2,
@@ -12,7 +12,7 @@ const CONSTANT_DATA = [
     {
         id: 3,
         image: 'https://cdn.pixabay.com/photo/2023/04/23/15/52/flowers-7946004_1280.jpg',
-        count: 11
+        count: 0
     },
 ];
 
